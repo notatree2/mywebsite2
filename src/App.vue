1da42ed77d88f09a9010b0d57ac9a1f10457cf2b
CrOS Man <juliswater@gmail.com>
2024-07-30 16:20:53 -0400

    fixed the buttons you had to click the text to be redirected

--- src/App.vue
+++ src/App.vue
@@ -56,9 +56,9 @@
   <div class="rounded-lg bg-neutral-100 p-10 text-center overflow-hidden shadow-xl shadow-slate-300/3 mt-24 mr-48 ml-48">
     <div class="text-pretty text-slate-600 leading-relaxed">
       <p>Hello, and welcome to the placeholder frontpage for my new website! You can check out the source if you wish. You can also check out the old website, or a project called Tech Independence Journey! Very heavily inspired off a <a href="https://www.youtube.com/watch?v=UHciHtsuFnw" class="text-blue-500">tutorial</a> (though i tried to write the code by myself)</p>
-        <div class=" grid columns-2 w-full rounded-xl p-5 font-medium">
-          <button class=" border rounded-lg bg-slate-300 p-3 transition-colors hover:bg-slate-400 active:bg-slate-500"><a :href="old" target="_blank">Old Website</a></button>
-          <button class=" p-3 border rounded-lg bg-blue-300 transition-colors hover:bg-blue-400 active:bg-blue-500"><a :href="website" target="_blank">Tech Independence Journey</a></button>
+        <div class=" grid rows-2 w-full rounded-xl p-5 font-medium">
+          <a :href="old" target="_blank"><button class="  w-full border rounded-lg bg-slate-300 p-3 transition-colors hover:bg-slate-400 active:bg-slate-500">Old Website</button></a>
+          <a :href="website" target="_blank"><button class=" w-full p-3 border rounded-lg bg-blue-300 transition-colors hover:bg-blue-400 active:bg-blue-500">Tech Independence Journey</button></a>
           
         </div>
         <p @click="changeme">{{statian}}</p>
