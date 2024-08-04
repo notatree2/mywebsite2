f363a5724b509f1a0e65081892c5464ed2227b90
CrOS Man <juliswater@gmail.com>
2024-08-04 19:01:52 -0400

    Added darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 
+
   import { ref } from "vue"
+  import { useDark, useToggle } from '@vueuse/core';
+  const isDark = ref(useDark())
   const website = ref("https://tech-independence-journey.codeberg.page/")
   const old = ref("https://notatree2.github.io/mywebsite/")
   const github = ref("src\assets\images\github.png")
@@ -53,19 +56,22 @@
 </script>
 
 <template>
-  <div class="rounded-lg bg-neutral-100 p-10 text-center overflow-hidden shadow-xl shadow-slate-300/3 mt-24 mr-48 ml-48">
-    <div class="text-pretty text-slate-600 leading-relaxed">
+<div class="place-content-center h-screen dark:bg-slate-700 z-0">
+  <div class="rounded-lg bg-neutral-100 p-10 text-center overflow-hidden shadow-xl shadow-slate-300/3 mr-48 ml-48 dark:bg-slate-800 z-10">
+    <div class="text-pretty text-slate-600 leading-relaxed dark:text-slate-400">
       <p>Hello, and welcome to the placeholder frontpage for my new website! You can check out the source if you wish. You can also check out the old website, or a project called Tech Independence Journey! Very heavily inspired off a <a href="https://www.youtube.com/watch?v=UHciHtsuFnw" class="text-blue-500">tutorial</a> (though i tried to write the code by myself)</p>
         <div class=" grid rows-2 w-full rounded-xl p-5 font-medium gap-2">
-          <a :href="old" target="_blank"><button class="  w-full border rounded-lg bg-slate-300 p-3 transition-colors hover:bg-slate-400 active:bg-slate-500">Old Website</button></a>
-          <a :href="website" target="_blank"><button class=" w-full p-3 border rounded-lg bg-blue-300 transition-colors hover:bg-blue-400 active:bg-blue-500">Tech Independence Journey</button></a>
+          <a :href="old" target="_blank"><button class="  w-full border rounded-lg bg-slate-300 p-3 transition-colors hover:bg-slate-400 active:bg-slate-500 dark:bg-slate-600">Old Website</button></a>
+          <a :href="website" target="_blank"><button class=" w-full p-3 border rounded-lg bg-blue-300 transition-colors hover:bg-blue-400 active:bg-blue-500 dark:text-slate-900">Tech Independence Journey</button></a>
           
         </div>
         <p @click="changeme" class="animation animate-bounce duration-400">{{statian}}</p>
+        <input type="checkbox" v-model="isDark">
       </div>
   
   
   </div>
+</div>
 </template>
 
 <style scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
-/* @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+
 export default {
+  darkMode: 'class',
   content:[
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -8,5 +10,6 @@ export default {
     extend: {},
   },
   plugins: [],
+
 }
 
