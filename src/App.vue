adecc880306f7545046ee53c811665f527ebcc22
CrOS Man <juliswater@gmail.com>
2024-08-19 14:15:10 -0400

    changed text, added a little heading at the top

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
   const isDark = ref(useDark())
   const website = ref("https://tech-independence-journey.codeberg.page/")
   const old = ref("https://notatree2.github.io/mywebsite/")
+  const source = ref("https://github.com/notatree2/mywebsite2")
   const github = ref("src\assets\images\github.png")
   const statian = ref("")
   // ---------------------------------------------------------------
@@ -60,10 +61,12 @@
 <template>
 <div class="place-content-center h-screen dark:bg-slate-700 z-0">
   <div class="rounded-lg bg-neutral-100 p-10 text-center overflow-hidden shadow-xl shadow-slate-300/3 mr-48 ml-48 dark:bg-slate-800 z-10">
-    <div class="text-pretty text-slate-600 leading-relaxed dark:text-slate-400">
-      <p>Hello, and welcome to the placeholder frontpage for my new website! You can check out the source if you wish. You can also check out the old website, or a project called Tech Independence Journey! Very heavily inspired off a <a href="https://www.youtube.com/watch?v=UHciHtsuFnw" class="text-blue-500">tutorial</a> (though i tried to write the code by myself)</p>
+    
+    <div class="text-pretty text-slate-600 leading-relaxed dark:text-slate-400 ">
+      <h1 class="font-bold text-3xl p-3 font-sans">Hello, i am notacupofmilk.</h1>
+      <p>I am a Christian, gamer, and i like the internet. This website right now is a placeholder for whats to come. The source code can be found <a :href="source" class="font-bold text-blue-400 hover:text-blue-500 transition transition-all active:text-blue-600">here.</a>  Very heavily inspired off a <a href="https://www.youtube.com/watch?v=UHciHtsuFnw" class="text-blue-500">tutorial</a> (though i tried to write the code by myself)</p>
       <br>
-        <div class="grid grid-cols-2 w-full rounded-xl p-5 font-medium gap-2">
+        <div class="grid grid-cols-2 w-full rounded-xl p-5 font-medium gap-2 ">
           <a :href="old" target="_blank"><button class="  w-full border rounded-lg bg-slate-300 p-3 transition-colors hover:bg-slate-400 active:bg-slate-500 dark:bg-slate-600">Old Website</button></a>
           <a :href="website" target="_blank"><button class=" w-full p-3 border rounded-lg bg-blue-300 transition-colors hover:bg-blue-400 active:bg-blue-500 dark:text-slate-900">Tech Independence Journey</button></a>
           
